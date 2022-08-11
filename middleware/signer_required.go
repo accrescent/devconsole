fc8504e8301cae25b08420ce1574c9f3ecdf573c
lberrymage <logan@lberrymage.dev>
2022-08-11 13:27:29 -0800

    Remove redundant DB read from SignerRequired middleware

--- middleware/signer_required.go
+++ middleware/signer_required.go
@@ -1,8 +1,6 @@
 package middleware
 
 import (
-	"database/sql"
-	"errors"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -12,24 +10,10 @@ import (
 
 func SignerRequired() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		db := c.MustGet("db").(*sql.DB)
 		conf := c.MustGet("config").(*config.Config)
-		sessionID := c.MustGet("session_id").(string)
+		ghID := c.MustGet("gh_id").(int)
 
-		var ghID int64
-		if err := db.QueryRow(
-			"SELECT gh_id FROM sessions WHERE id = ?",
-			sessionID,
-		).Scan(&ghID); err != nil {
-			if errors.Is(err, sql.ErrNoRows) {
-				_ = c.AbortWithError(http.StatusUnauthorized, err)
-			} else {
-				_ = c.AbortWithError(http.StatusInternalServerError, err)
-			}
-			return
-		}
-
-		if ghID != conf.SignerGitHubID {
+		if int64(ghID) != conf.SignerGitHubID {
 			c.AbortWithStatus(http.StatusForbidden)
 			return
 		}
