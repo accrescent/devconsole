65df211a4d7e1e55d34cd2286d444b09cfea9d0d
lberrymage <logan@lberrymage.dev>
2022-09-14 08:51:04 -0800

    Use slices.Contains for review checks
    This is much easier to read than a fallthrough switch.

--- devportal/quality/review_tests.go
+++ devportal/quality/review_tests.go
@@ -1,6 +1,33 @@
 package quality
 
-import "github.com/accrescent/apkstat"
+import (
+	"github.com/accrescent/apkstat"
+	"golang.org/x/exp/slices"
+)
+
+var permissionReviewBlacklist = []string{
+	"android.permission.ACCESS_BACKGROUND_LOCATION",
+	"android.permission.ACCESS_COARSE_LOCATION",
+	"android.permission.ACCESS_FINE_LOCATION",
+	"android.permission.BLUETOOTH_SCAN",
+	"android.permission.MANAGE_EXTERNAL_STORAGE",
+	"android.permission.NEARBY_WIFI_DEVICES",
+	"android.permission.PROCESS_OUTGOING_CALLS",
+	"android.permission.QUERY_ALL_PACKAGES",
+	"android.permission.READ_CALL_LOG",
+	"android.permission.READ_EXTERNAL_STORAGE",
+	"android.permission.READ_PHONE_STATE",
+	"android.permission.READ_MEDIA_AUDIO",
+	"android.permission.READ_MEDIA_IMAGES",
+	"android.permission.READ_MEDIA_VIDEO",
+	"android.permission.READ_SMS",
+	"android.permission.RECEIVE_MMS",
+	"android.permission.RECEIVE_SMS",
+	"android.permission.RECEIVE_WAP_PUSH",
+	"android.permission.REQUEST_INSTALL_PACKAGES",
+	"android.permission.SEND_SMS",
+	"android.permission.WRITE_CALL_LOG",
+}
 
 func RunReviewTests(apk *apk.APK) []string {
 	// We don't want this slice to ever be nil because returning it as JSON would result in a
@@ -12,48 +39,7 @@ func RunReviewTests(apk *apk.APK) []string {
 
 	if permissions != nil {
 		for _, permission := range *permissions {
-			switch permission.Name {
-			case "android.permission.ACCESS_BACKGROUND_LOCATION":
-				fallthrough
-			case "android.permission.ACCESS_COARSE_LOCATION":
-				fallthrough
-			case "android.permission.ACCESS_FINE_LOCATION":
-				fallthrough
-			case "android.permission.BLUETOOTH_SCAN":
-				fallthrough
-			case "android.permission.MANAGE_EXTERNAL_STORAGE":
-				fallthrough
-			case "android.permission.NEARBY_WIFI_DEVICES":
-				fallthrough
-			case "android.permission.PROCESS_OUTGOING_CALLS":
-				fallthrough
-			case "android.permission.QUERY_ALL_PACKAGES":
-				fallthrough
-			case "android.permission.READ_CALL_LOG":
-				fallthrough
-			case "android.permission.READ_EXTERNAL_STORAGE":
-				fallthrough
-			case "android.permission.READ_PHONE_STATE":
-				fallthrough
-			case "android.permission.READ_MEDIA_AUDIO":
-				fallthrough
-			case "android.permission.READ_MEDIA_IMAGES":
-				fallthrough
-			case "android.permission.READ_MEDIA_VIDEO":
-				fallthrough
-			case "android.permission.READ_SMS":
-				fallthrough
-			case "android.permission.RECEIVE_MMS":
-				fallthrough
-			case "android.permission.RECEIVE_SMS":
-				fallthrough
-			case "android.permission.RECEIVE_WAP_PUSH":
-				fallthrough
-			case "android.permission.REQUEST_INSTALL_PACKAGES":
-				fallthrough
-			case "android.permission.SEND_SMS":
-				fallthrough
-			case "android.permission.WRITE_CALL_LOG":
+			if slices.Contains(permissionReviewBlacklist, permission.Name) {
 				errors = append(errors, permission.Name)
 			}
 		}
