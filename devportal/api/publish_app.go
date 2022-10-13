0faa8165350b3d5b711f0aef686dd9a50c6c738d
lberrymage <logan@lberrymage.dev>
2022-10-12 21:57:09 -0800

    Rename `published_user_apps` table to `user_permissions`

--- devportal/api/publish_app.go
+++ devportal/api/publish_app.go
@@ -46,7 +46,7 @@ func PublishApp(c *gin.Context) {
 		return
 	}
 	if _, err := tx.Exec(
-		"INSERT INTO published_app_users (app_id, user_gh_id) VALUES (?, ?)",
+		"INSERT INTO user_permissions (app_id, user_gh_id) VALUES (?, ?)",
 		appID, ghID,
 	); err != nil {
 		if errors.Is(err.(sqlite3.Error).ExtendedCode, sqlite3.ErrConstraintPrimaryKey) {
