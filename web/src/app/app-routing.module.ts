b532528ec02b054f67b300ee54c69a73ab2d1aed
lberrymage <logan@lberrymage.dev>
2022-10-26 14:12:36 -0800

    Re-implement landing page in Angular

--- web/src/app/app-routing.module.ts
+++ web/src/app/app-routing.module.ts
@@ -2,8 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { RegisterComponent } from './register/register.component';
+import { LandingComponent } from './landing/landing.component';
 
 const routes: Routes = [
+    { path: '', component: LandingComponent },
     { path: 'register', component: RegisterComponent }
 ];
 
---
+++ web/src/app/landing/landing.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-landing',
+    templateUrl: './landing.component.html',
+    styleUrls: ['./landing.component.css']
+})
+export class LandingComponent {}
