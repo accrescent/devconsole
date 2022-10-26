72251a741ed8a38781ea081231c78ade5719ba2f
lberrymage <logan@lberrymage.dev>
2022-10-26 14:12:36 -0800

    Re-implement landing page in Angular

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -7,12 +7,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RegisterComponent } from './register/register.component';
 import { RegisterFormComponent } from './register-form/register-form.component';
+import { LandingComponent } from './landing/landing.component';
 
 @NgModule({
     declarations: [
         AppComponent,
         RegisterComponent,
-        RegisterFormComponent
+        RegisterFormComponent,
+        LandingComponent
     ],
     imports: [
         BrowserModule,
