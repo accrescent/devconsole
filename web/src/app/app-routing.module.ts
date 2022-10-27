e6bf12d424f4801f845a6e636115aaf09d84df16
lberrymage <logan@lberrymage.dev>
2022-10-27 13:08:54 -0800

    Re-implement partial dashboard page
    Also add a stub page for new app uploads (to be implemented later) so
    the dashboard has something to link to.

--- web/src/app/app-routing.module.ts
+++ web/src/app/app-routing.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { NewAppComponent } from './new-app/new-app.component';
 import { RegisterComponent } from './register/register.component';
 import { LandingComponent } from './landing/landing.component';
 
 const routes: Routes = [
     { path: '', component: LandingComponent },
-    { path: 'register', component: RegisterComponent }
+    { path: 'dashboard', component: DashboardComponent },
+    { path: 'register', component: RegisterComponent },
+    { path: 'new-app', component: NewAppComponent },
 ];
 
 @NgModule({
---
+++ web/src/app/new-app/new-app.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-new-app',
+    templateUrl: './new-app.component.html',
+    styleUrls: ['./new-app.component.css']
+})
+export class NewAppComponent {}
