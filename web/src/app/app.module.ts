49c35d3371952af220dfb0edc4796181438041d6
lberrymage <logan@lberrymage.dev>
2022-10-26 21:03:32 -0800

    Add basic Material styling to website
    Closes #17

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -2,6 +2,9 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
+import { MatRadioModule } from '@angular/material/radio';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -19,6 +22,9 @@ import { LandingComponent } from './landing/landing.component';
     imports: [
         BrowserModule,
         HttpClientModule,
+        BrowserAnimationsModule,
+        MatButtonModule,
+        MatRadioModule,
         ReactiveFormsModule,
         AppRoutingModule
     ],
