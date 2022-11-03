cf305e8af49c62f7ba204cd01e255a621cb6a069
lberrymage <logan@lberrymage.dev>
2022-11-02 21:34:17 -0800

    Implement new app review approval

--- web/src/app/review/review.component.ts
+++ web/src/app/review/review.component.ts
@@ -16,4 +16,13 @@ export class ReviewComponent implements OnInit {
     ngOnInit(): void {
         this.appService.getPendingApps().subscribe(apps => this.apps = apps);
     }
+
+    approveApp(appId: string): void {
+        this.appService.approveApp(appId).subscribe(_ => {
+            const i = this.apps.findIndex(a => a.app_id === appId);
+            if (i > -1) {
+                this.apps.splice(i, 1);
+            }
+        });
+    }
 }
