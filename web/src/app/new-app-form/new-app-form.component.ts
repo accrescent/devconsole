960f9008720e5fb826556644499752d16485ddcf
lberrymage <logan@lberrymage.dev>
2022-11-02 23:07:51 -0800

    Remove extraneous `Validators` import in new app form

--- web/src/app/new-app-form/new-app-form.component.ts
+++ web/src/app/new-app-form/new-app-form.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NonNullableFormBuilder, Validators } from '@angular/forms';
+import { NonNullableFormBuilder } from '@angular/forms';
 import { Router } from '@angular/router';
 
 import { App } from '../app';
