3f818329d16c686cf1268fde9393b5b3e236eb99
lberrymage <logan@lberrymage.dev>
2022-11-07 15:17:57 -0900

    Use named return parameter for RunReviewTests
    Originally we explicitly didn't use a named return parameter since
    handling a null JSON value required extra code and so was more work to
    handle. However, this is no longer the case since switching to Angular
    for our frontend, so we can remove the redundancy in favor of a slight
    increase in code readability.

--- devportal/quality/review_tests.go
+++ devportal/quality/review_tests.go
@@ -5,12 +5,7 @@ import (
 	"golang.org/x/exp/slices"
 )
 
-func RunReviewTests(apk *apk.APK) []string {
-	// We don't want this slice to ever be nil because returning it as JSON would result in a
-	// null value in JavaScript which is an unnecessary extra case we would need to handle since
-	// null isn't iterable. See https://github.com/gin-gonic/gin/issues/125.
-	issues := []string{}
-
+func RunReviewTests(apk *apk.APK) (issues []string) {
 	permissions := apk.Manifest().UsesPermissions
 
 	if permissions != nil {
