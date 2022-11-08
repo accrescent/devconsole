08ae96f544ac9b31d87f3de7e926207da2ee9bd3
lberrymage <logan@lberrymage.dev>
2022-11-07 15:06:02 -0900

    Rename "errors" variable to "issues" in review tests
    This was missed in d989669.

--- devportal/quality/review_tests.go
+++ devportal/quality/review_tests.go
@@ -9,17 +9,17 @@ func RunReviewTests(apk *apk.APK) []string {
 	// We don't want this slice to ever be nil because returning it as JSON would result in a
 	// null value in JavaScript which is an unnecessary extra case we would need to handle since
 	// null isn't iterable. See https://github.com/gin-gonic/gin/issues/125.
-	errors := []string{}
+	issues := []string{}
 
 	permissions := apk.Manifest().UsesPermissions
 
 	if permissions != nil {
 		for _, permission := range *permissions {
 			if slices.Contains(permissionReviewBlacklist, permission.Name) {
-				errors = append(errors, permission.Name)
+				issues = append(issues, permission.Name)
 			}
 		}
 	}
 
-	return errors
+	return issues
 }
