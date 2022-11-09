c2025f04f6aa38af77d888bc6101e0a3fd7e8a73
lberrymage <logan@lberrymage.dev>
2022-11-09 06:24:45 -0900

    Spruce up login page

--- web/src/app/landing/landing.component.ts
+++ web/src/app/landing/landing.component.ts
@@ -3,5 +3,6 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'app-landing',
     templateUrl: './landing.component.html',
+    styleUrls: ['./landing.component.css'],
 })
 export class LandingComponent {}
