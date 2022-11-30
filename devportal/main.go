85b4c92e8169fc43a9c058c7100a0b26e834da89
lberrymage <logan@lberrymage.dev>
2022-11-30 15:42:00 -0700

    Encapsulate app into separate struct
    This allows for easier swapping of component implementations, which is
    particularly useful for testing.

--- devportal/main.go
+++ devportal/main.go
@@ -11,32 +11,17 @@ import (
 	"syscall"
 	"time"
 
-	"github.com/gin-gonic/gin"
 	"golang.org/x/oauth2"
 	"golang.org/x/oauth2/endpoints"
 
-	"github.com/accrescent/devportal/api"
-	"github.com/accrescent/devportal/auth"
 	"github.com/accrescent/devportal/config"
 	"github.com/accrescent/devportal/data"
-	"github.com/accrescent/devportal/middleware"
 )
 
 func main() {
-	r := gin.New()
-	r.Use(gin.Logger())
-	if err := r.SetTrustedProxies(nil); err != nil {
-		log.Fatal(err)
-	}
-
 	db := new(data.SQLite)
-	if err := db.Open("devportal.db?_fk=yes&_journal=WAL"); err != nil {
-		log.Fatal(err)
-	}
-	defer db.Close()
-	if err := db.Initialize(); err != nil {
-		log.Fatal(err)
-	}
+
+	fileStorage := data.NewLocalStorage("/")
 
 	oauth2Conf := oauth2.Config{
 		ClientID:     os.Getenv("GH_CLIENT_ID"),
@@ -56,43 +41,13 @@ func main() {
 		APIKey:         os.Getenv("API_KEY"),
 	}
 
-	r.Use(middleware.DB(db))
-	r.Use(middleware.FileStorage(data.NewLocalStorage("/")))
-	r.Use(middleware.OAuth2Config(oauth2Conf))
-	r.Use(middleware.Config(conf))
-
-	r.GET("/auth/github", auth.GitHub)
-	r.GET("/api/auth/github/callback", auth.GitHubCallback)
-
-	auth := r.Group("/", middleware.AuthRequired())
-	reviewer := auth.Group("/", middleware.ReviewerRequired())
-	update := auth.Group("/", middleware.UserCanUpdateRequired())
-	auth.GET("/api/emails", api.GetEmails)
-	reviewer.GET("/api/pending-apps", api.GetPendingApps)
-	reviewer.GET("/api/pending-apps/:id/apks", api.GetAppAPKs)
-	reviewer.PATCH("/api/pending-apps/:id", api.ApproveApp)
-	reviewer.DELETE("/api/pending-apps/:id", api.RejectApp)
-	reviewer.GET("/api/updates", api.GetUpdates)
-	reviewer.GET("/api/updates/:id/:version/apks", api.GetUpdateAPKs)
-	reviewer.PATCH("/api/updates/:id/:version", api.ApproveUpdate)
-	reviewer.DELETE("/api/updates/:id/:version", api.RejectUpdate)
-	auth.GET("/api/approved-apps", middleware.SignerRequired(), api.GetApprovedApps)
-	auth.POST("/api/register", api.Register)
-	auth.DELETE("/api/session", api.LogOut)
-	auth.GET("/api/apps", api.GetApps)
-	auth.POST("/api/apps", api.NewApp)
-	auth.PATCH("/api/apps/:id", api.SubmitApp)
-	update.POST("/api/apps/:id/updates", api.NewUpdate)
-	update.PATCH("/api/apps/:id/:version", api.SubmitUpdate)
-	auth.POST("/api/apps/:id", middleware.SignerRequired(), api.PublishApp)
-
-	srv := &http.Server{
-		Addr:    ":8080",
-		Handler: r,
+	app, err := NewApp(db, "devportal.db?_fk=yes&_journal=WAL", fileStorage, oauth2Conf, conf)
+	if err != nil {
+		log.Fatal(err)
 	}
 
 	go func() {
-		if err := srv.ListenAndServe(); err != nil && errors.Is(http.ErrServerClosed, err) {
+		if err := app.Start(); err != nil && errors.Is(http.ErrServerClosed, err) {
 			log.Println(err)
 		}
 	}()
@@ -104,7 +59,7 @@ func main() {
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
-	if err := srv.Shutdown(ctx); err != nil {
+	if err := app.Stop(ctx); err != nil {
 		log.Fatal("Shutting down forcefully:", err)
 	}
 }
