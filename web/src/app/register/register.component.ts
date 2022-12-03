a84cc02b1df451bd200730a6d1a950ff0fc248b1
lberrymage <logan@lberrymage.dev>
2022-12-02 17:34:28 -0700

    Add padding to registration page

--- web/src/app/register/register.component.ts
+++ web/src/app/register/register.component.ts
@@ -3,5 +3,6 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'app-register',
     templateUrl: './register.component.html',
+    styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent {}
