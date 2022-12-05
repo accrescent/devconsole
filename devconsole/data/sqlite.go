eba0d73cd683dfdaddb0dfc40d80ac243fc9d793
lberrymage <logan@lberrymage.dev>
2022-12-05 15:49:15 -0700

    Set `PRAGMA user_version` in SQLite database
    This allows us to set up migrations later.

--- devconsole/data/sqlite.go
+++ devconsole/data/sqlite.go
@@ -41,119 +41,130 @@ func (s *SQLite) Open(dsn string) error {
 }
 
 func (s *SQLite) Initialize() error {
-	if _, err := s.db.Exec(`CREATE TABLE IF NOT EXISTS config (
-		key TEXT PRIMARY KEY,
-		value TEXT NOT NULL
-	) STRICT`); err != nil {
+	var userVersion int
+	if err := s.db.QueryRow("PRAGMA user_version").Scan(&userVersion); err != nil {
 		return err
 	}
-	if _, err := s.db.Exec(`CREATE TABLE IF NOT EXISTS sessions (
-		id TEXT PRIMARY KEY,
-		gh_id INT NOT NULL,
-		access_token TEXT NOT NULL,
-		expiry_time INT NOT NULL
-	) STRICT`); err != nil {
-		return err
-	}
-	if _, err := s.db.Exec(`CREATE TABLE IF NOT EXISTS users (
-		gh_id INT PRIMARY KEY,
-		email TEXT NOT NULL
-	) STRICT`); err != nil {
-		return err
-	}
-	if _, err := s.db.Exec(`CREATE TABLE IF NOT EXISTS reviewers (
-		user_gh_id INT PRIMARY KEY REFERENCES users(gh_id) ON DELETE CASCADE,
-		email TEXT NOT NULL
-	) STRICT`); err != nil {
-		return err
-	}
-	if _, err := s.db.Exec(`CREATE TABLE IF NOT EXISTS staging_apps (
-		id TEXT NOT NULL,
-		user_gh_id INT NOT NULL REFERENCES users(gh_id) ON DELETE CASCADE,
-		label TEXT NOT NULL,
-		version_code INT NOT NULL,
-		version_name TEXT NOT NULL,
-		file_handle TEXT NOT NULL,
-		icon_id INT NOT NULL REFERENCES icons(id),
-		issue_group_id INT REFERENCES issue_groups(id),
-		PRIMARY KEY (id, user_gh_id)
-	) STRICT`); err != nil {
-		return err
-	}
-	if _, err := s.db.Exec(`CREATE TABLE IF NOT EXISTS icons (
-		id INTEGER PRIMARY KEY,
-		file_handle TEXT NOT NULL,
-		hash TEXT NOT NULL
-	) STRICT`); err != nil {
-		return err
-	}
-	if _, err := s.db.Exec(`CREATE TABLE IF NOT EXISTS issue_groups (
-		id INTEGER PRIMARY KEY
-	) STRICT`); err != nil {
-		return err
-	}
-	if _, err := s.db.Exec(`CREATE TABLE IF NOT EXISTS issues (
-		id TEXT NOT NULL,
-		issue_group_id INT NOT NULL REFERENCES issue_groups(id) ON DELETE CASCADE,
-		PRIMARY KEY (id, issue_group_id)
-	) STRICT`); err != nil {
-		return err
-	}
-	if _, err := s.db.Exec(`CREATE TABLE IF NOT EXISTS submitted_apps (
-		id TEXT PRIMARY KEY,
-		gh_id INT NOT NULL REFERENCES users(gh_id) ON DELETE CASCADE,
-		label TEXT NOT NULL,
-		version_code INT NOT NULL,
-		version_name TEXT NOT NULL,
-		icon_id INT NOT NULL REFERENCES icons(id),
-		issue_group_id INT REFERENCES issue_groups(id),
-		reviewer_gh_id INT NOT NULL REFERENCES reviewers(user_gh_id),
-		approved INT NOT NULL CHECK(approved in (FALSE, TRUE)) DEFAULT FALSE,
-		file_handle TEXT NOT NULL
-	) STRICT`); err != nil {
-		return err
-	}
-	if _, err := s.db.Exec(`CREATE TABLE IF NOT EXISTS published_apps (
-		id TEXT PRIMARY KEY,
-		label TEXT NOT NULL,
-		version_code INT NOT NULL,
-		version_name TEXT NOT NULL,
-		icon_id INT NOT NULL REFERENCES icons(id),
-		issue_group_id INT REFERENCES issue_groups(id)
-	) STRICT`); err != nil {
-		return err
-	}
-	if _, err := s.db.Exec(`CREATE TABLE IF NOT EXISTS user_permissions (
-		app_id TEXT NOT NULL REFERENCES published_apps(id) ON DELETE CASCADE,
-		user_gh_id INT NOT NULL REFERENCES users(gh_id) ON DELETE CASCADE,
-		can_update INT NOT NULL CHECK(can_update in (FALSE, TRUE)) DEFAULT FALSE,
-		PRIMARY KEY (app_id, user_gh_id)
-	) STRICT`); err != nil {
-		return err
-	}
-	if _, err := s.db.Exec(`CREATE TABLE IF NOT EXISTS staging_updates (
-		app_id TEXT NOT NULL REFERENCES published_apps(id) ON DELETE CASCADE,
-		user_gh_id INT NOT NULL REFERENCES users(gh_id) ON DELETE CASCADE,
-		label TEXT NOT NULL,
-		version_code INT NOT NULL,
-		version_name TEXT NOT NULL,
-		file_handle TEXT NOT NULL,
-		issue_group_id INT REFERENCES issue_groups(id),
-		PRIMARY KEY (app_id, version_code)
-	) STRICT`); err != nil {
-		return err
-	}
-	if _, err := s.db.Exec(`CREATE TABLE IF NOT EXISTS submitted_updates (
-		app_id TEXT NOT NULL REFERENCES published_apps(id) ON DELETE CASCADE,
-		label TEXT NOT NULL,
-		version_code INT NOT NULL,
-		version_name TEXT NOT NULL,
-		reviewer_gh_id INT NOT NULL REFERENCES reviewers(user_gh_id),
-		file_handle TEXT NOT NULL,
-		issue_group_id INT NOT NULL REFERENCES issue_groups(id),
-		PRIMARY KEY (app_id, version_code)
-	) STRICT`); err != nil {
-		return err
+
+	if userVersion < 1 {
+		if _, err := s.db.Exec(`CREATE TABLE config (
+			key TEXT PRIMARY KEY,
+			value TEXT NOT NULL
+		) STRICT`); err != nil {
+			return err
+		}
+		if _, err := s.db.Exec(`CREATE TABLE sessions (
+			id TEXT PRIMARY KEY,
+			gh_id INT NOT NULL,
+			access_token TEXT NOT NULL,
+			expiry_time INT NOT NULL
+		) STRICT`); err != nil {
+			return err
+		}
+		if _, err := s.db.Exec(`CREATE TABLE users (
+			gh_id INT PRIMARY KEY,
+			email TEXT NOT NULL
+		) STRICT`); err != nil {
+			return err
+		}
+		if _, err := s.db.Exec(`CREATE TABLE reviewers (
+			user_gh_id INT PRIMARY KEY REFERENCES users(gh_id) ON DELETE CASCADE,
+			email TEXT NOT NULL
+		) STRICT`); err != nil {
+			return err
+		}
+		if _, err := s.db.Exec(`CREATE TABLE staging_apps (
+			id TEXT NOT NULL,
+			user_gh_id INT NOT NULL REFERENCES users(gh_id) ON DELETE CASCADE,
+			label TEXT NOT NULL,
+			version_code INT NOT NULL,
+			version_name TEXT NOT NULL,
+			file_handle TEXT NOT NULL,
+			icon_id INT NOT NULL REFERENCES icons(id),
+			issue_group_id INT REFERENCES issue_groups(id),
+			PRIMARY KEY (id, user_gh_id)
+		) STRICT`); err != nil {
+			return err
+		}
+		if _, err := s.db.Exec(`CREATE TABLE icons (
+			id INTEGER PRIMARY KEY,
+			file_handle TEXT NOT NULL,
+			hash TEXT NOT NULL
+		) STRICT`); err != nil {
+			return err
+		}
+		if _, err := s.db.Exec(`CREATE TABLE issue_groups (
+			id INTEGER PRIMARY KEY
+		) STRICT`); err != nil {
+			return err
+		}
+		if _, err := s.db.Exec(`CREATE TABLE issues (
+			id TEXT NOT NULL,
+			issue_group_id INT NOT NULL REFERENCES issue_groups(id) ON DELETE CASCADE,
+			PRIMARY KEY (id, issue_group_id)
+		) STRICT`); err != nil {
+			return err
+		}
+		if _, err := s.db.Exec(`CREATE TABLE submitted_apps (
+			id TEXT PRIMARY KEY,
+			gh_id INT NOT NULL REFERENCES users(gh_id) ON DELETE CASCADE,
+			label TEXT NOT NULL,
+			version_code INT NOT NULL,
+			version_name TEXT NOT NULL,
+			icon_id INT NOT NULL REFERENCES icons(id),
+			issue_group_id INT REFERENCES issue_groups(id),
+			reviewer_gh_id INT NOT NULL REFERENCES reviewers(user_gh_id),
+			approved INT NOT NULL CHECK(approved in (FALSE, TRUE)) DEFAULT FALSE,
+			file_handle TEXT NOT NULL
+		) STRICT`); err != nil {
+			return err
+		}
+		if _, err := s.db.Exec(`CREATE TABLE published_apps (
+			id TEXT PRIMARY KEY,
+			label TEXT NOT NULL,
+			version_code INT NOT NULL,
+			version_name TEXT NOT NULL,
+			icon_id INT NOT NULL REFERENCES icons(id),
+			issue_group_id INT REFERENCES issue_groups(id)
+		) STRICT`); err != nil {
+			return err
+		}
+		if _, err := s.db.Exec(`CREATE TABLE user_permissions (
+			app_id TEXT NOT NULL REFERENCES published_apps(id) ON DELETE CASCADE,
+			user_gh_id INT NOT NULL REFERENCES users(gh_id) ON DELETE CASCADE,
+			can_update INT NOT NULL CHECK(can_update in (FALSE, TRUE)) DEFAULT FALSE,
+			PRIMARY KEY (app_id, user_gh_id)
+		) STRICT`); err != nil {
+			return err
+		}
+		if _, err := s.db.Exec(`CREATE TABLE staging_updates (
+			app_id TEXT NOT NULL REFERENCES published_apps(id) ON DELETE CASCADE,
+			user_gh_id INT NOT NULL REFERENCES users(gh_id) ON DELETE CASCADE,
+			label TEXT NOT NULL,
+			version_code INT NOT NULL,
+			version_name TEXT NOT NULL,
+			file_handle TEXT NOT NULL,
+			issue_group_id INT REFERENCES issue_groups(id),
+			PRIMARY KEY (app_id, version_code)
+		) STRICT`); err != nil {
+			return err
+		}
+		if _, err := s.db.Exec(`CREATE TABLE submitted_updates (
+			app_id TEXT NOT NULL REFERENCES published_apps(id) ON DELETE CASCADE,
+			label TEXT NOT NULL,
+			version_code INT NOT NULL,
+			version_name TEXT NOT NULL,
+			reviewer_gh_id INT NOT NULL REFERENCES reviewers(user_gh_id),
+			file_handle TEXT NOT NULL,
+			issue_group_id INT NOT NULL REFERENCES issue_groups(id),
+			PRIMARY KEY (app_id, version_code)
+		) STRICT`); err != nil {
+			return err
+		}
+
+		if _, err := s.db.Exec("PRAGMA user_version = 1"); err != nil {
+			return err
+		}
 	}
 
 	return nil
