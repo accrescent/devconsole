accrescent/devconsole

commit 7eb8741000acec41bf8ab5d89a8ba9eda5377b04

Use `nil` for empty slice values
This way is more concise.

reposerver/api/publish.go
@@ -40,9 +40,9 @@ func publish(c *gin.Context, uploadType uploadType) {
 	repoData := repoData{
 		Version:       version,
 		VersionCode:   versionCodeInt,
-		ABISplits:     []string{},
-		DensitySplits: []string{},
-		LangSplits:    []string{},
+		ABISplits:     nil,
+		DensitySplits: nil,
+		LangSplits:    nil,
 	}
 
 	// Extract APKs from APK set

devportal/data/sqlite.go
@@ -321,7 +321,7 @@ func (s *SQLite) GetApprovedApps() ([]App, error) {
 		WHERE approved = TRUE`,
 	)
 	if err != nil {
-		return []App{}, err
+		return nil, err
 	}
 	defer rows.Close()
 	var apps []App
@@ -329,7 +329,7 @@ func (s *SQLite) GetApprovedApps() ([]App, error) {
 		var appID, label, versionName string
 		var versionCode int32
 		if err := rows.Scan(&appID, &label, &versionCode, &versionName); err != nil {
-			return []App{}, err
+			return nil, err
 		}
 
 		app := App{appID, label, versionCode, versionName}
@@ -349,7 +349,7 @@ func (s *SQLite) GetApps(ghID int64) ([]App, error) {
 		ghID,
 	)
 	if err != nil {
-		return []App{}, err
+		return nil, err
 	}
 	defer rows.Close()
 	var apps []App
@@ -357,7 +357,7 @@ func (s *SQLite) GetApps(ghID int64) ([]App, error) {
 		var appID, label, versionName string
 		var versionCode int32
 		if err := rows.Scan(&appID, &label, &versionCode, &versionName); err != nil {
-			return []App{}, err
+			return nil, err
 		}
 
 		app := App{appID, label, versionCode, versionName}
@@ -375,7 +375,7 @@ func (s *SQLite) GetPendingApps(reviewerGhID int64) ([]AppWithIssues, error) {
 		reviewerGhID,
 	)
 	if err != nil {
-		return []AppWithIssues{}, err
+		return nil, err
 	}
 	defer dbApps.Close()
 	var apps []AppWithIssues
@@ -390,7 +390,7 @@ func (s *SQLite) GetPendingApps(reviewerGhID int64) ([]AppWithIssues, error) {
 			&versionName,
 			&issueGroupID,
 		); err != nil {
-			return []AppWithIssues{}, err
+			return nil, err
 		}
 
 		dbIssues, err := s.db.Query(
@@ -398,14 +398,14 @@ func (s *SQLite) GetPendingApps(reviewerGhID int64) ([]AppWithIssues, error) {
 			issueGroupID,
 		)
 		if err != nil {
-			return []AppWithIssues{}, err
+			return nil, err
 		}
 		defer dbIssues.Close()
 		var issues []string
 		for dbIssues.Next() {
 			var issue string
 			if err := dbIssues.Scan(&issue); err != nil {
-				return []AppWithIssues{}, err
+				return nil, err
 			}
 
 			issues = append(issues, issue)
@@ -633,7 +633,7 @@ func (s *SQLite) GetUpdates(reviewerGhID int64) ([]AppWithIssues, error) {
 		reviewerGhID,
 	)
 	if err != nil {
-		return []AppWithIssues{}, err
+		return nil, err
 	}
 	defer dbApps.Close()
 	var apps []AppWithIssues
@@ -648,7 +648,7 @@ func (s *SQLite) GetUpdates(reviewerGhID int64) ([]AppWithIssues, error) {
 			&versionName,
 			&issueGroupID,
 		); err != nil {
-			return []AppWithIssues{}, err
+			return nil, err
 		}
 
 		dbIssues, err := s.db.Query(
@@ -656,14 +656,14 @@ func (s *SQLite) GetUpdates(reviewerGhID int64) ([]AppWithIssues, error) {
 			issueGroupID,
 		)
 		if err != nil {
-			return []AppWithIssues{}, err
+			return nil, err
 		}
 		defer dbIssues.Close()
 		var issues []string
 		for dbIssues.Next() {
 			var issue string
 			if err := dbIssues.Scan(&issue); err != nil {
-				return []AppWithIssues{}, err
+				return nil, err
 			}
 
 			issues = append(issues, issue)

commit c1571d0186baa5394578789394fbe205789bd175

repo: Set repodata slice fields to empty instead of nil
When these repository metadata struct fields are nil, they are
serialized as null JSON values on the wire. The client doesn't know how
to handle this and so sees the metadata as invalid. It only understands
empty arrays.

This used to work fine but was accidentally broken by "7eb8741: Use
`nil` for empty slice values". To prevent the same mistake from
happening again, a comment has been added to the relevant code
explaining why empty slices are used.

reposerver/api/publish.go
@@ -39,11 +39,13 @@ func publish(c *gin.Context, uploadType uploadType) {
 	}
 
 	repoData := repoData{
-		Version:       version,
-		VersionCode:   versionCodeInt,
-		ABISplits:     nil,
-		DensitySplits: nil,
-		LangSplits:    nil,
+		Version:     version,
+		VersionCode: versionCodeInt,
+		// These slices are intentionally empty instead of nil so the serialized repository
+		// metadata is valid.
+		ABISplits:     []string{},
+		DensitySplits: []string{},
+		LangSplits:    []string{},
 	}
 
 	// Extract APKs from APK set